c813022cd112af5b5a84160fed0fde25987258d1
KongWooJeong <coolsteve@naver.com>
2023-05-31 02:49:25 +0900

    feat: Add 상품리스트 영역 스타일 추가 및 설정

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import menuIcon from "../../../public/icon/menu.svg";
 import cartIcon from "../../../public/icon/cart.svg";
 
+import { ProductItem } from "@/components/shop/product/ProductItem";
+
 export default function Shop() {
   return (
     <div className={styles["page-container"]}>
@@ -20,7 +22,13 @@ export default function Shop() {
           <p>사이토카인.</p>
           <p>완벅한 탈모케어를 위한 선택</p>
         </div>
-        <section className={styles["product-box"]}></section>
+        <section className={styles["product-box"]}>
+          {/* 스타일 설정을 위한 임시 아이템 선언, api 호출 작업시 변경 예정 */}
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+        </section>
       </main>
     </div>
   );
