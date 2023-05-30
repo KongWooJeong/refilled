d31b67b30f82a88bc2a55f4fcb56248130719f88
KongWooJeong <coolsteve@naver.com>
2023-05-30 17:44:42 +0900

    feat: Update lint & prettier 를 기존코드에 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
 export const metadata = {
-  title: '',
-  description: '',
-}
+  title: "",
+  description: "",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
