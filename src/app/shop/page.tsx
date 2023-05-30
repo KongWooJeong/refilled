187dcf131458f9e208cca7980f23f24159009a0a
KongWooJeong <coolsteve@naver.com>
2023-05-30 23:48:08 +0900

    feat: Add 헤더 영역 메뉴, 카드 아이콘 적용 및 스타일 수정

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,12 +1,19 @@
 import styles from "./shop.module.scss";
+import Image from "next/image";
+import menuIcon from "../../../public/icon/menu.svg";
+import cartIcon from "../../../public/icon/cart.svg";
 
 export default function Shop() {
   return (
     <div className={styles["page-container"]}>
       <header className={styles["header-container"]}>
-        <div className={styles["menu-box"]}>메뉴</div>
+        <div className={styles["menu-box"]}>
+          <Image src={menuIcon} alt="menu" />
+        </div>
         <div className={styles["title-box"]}>Refilled</div>
-        <div className={styles["cart-box"]}>장바구니</div>
+        <div className={styles["cart-box"]}>
+          <Image src={cartIcon} alt="cart" />
+        </div>
       </header>
       <main></main>
     </div>
