fafc3f76c603673765276dd9c2e9be8c07f6059e
KongWooJeong <coolsteve@naver.com>
2023-05-30 23:20:02 +0900

    feat: Add 헤더 영역 레이아웃 구성

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -3,7 +3,11 @@ import styles from "./shop.module.scss";
 export default function Shop() {
   return (
     <div className={styles["page-container"]}>
-      <header></header>
+      <header className={styles["header-container"]}>
+        <div className={styles["menu-box"]}>메뉴</div>
+        <div className={styles["title-box"]}>Refilled</div>
+        <div className={styles["cart-box"]}>장바구니</div>
+      </header>
       <main></main>
     </div>
   );
