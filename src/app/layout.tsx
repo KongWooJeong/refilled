93c5a32b2c9e7175b0b5dccae5aa64e3c0a5e2b4
KongWooJeong <coolsteve@naver.com>
2023-05-31 16:07:27 +0900

    feat: Add 레이아웃 구성 변경 (헤더 부분은 루트레이아웃으로 이동하여 적용) 그리고 scss 스타일 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import "normalize.css";
 
+import { Header } from "@/components/common/Header";
+import styles from "./rootLayout.module.scss";
+
 export const metadata = {
   title: "",
   description: "",
@@ -12,7 +15,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className={styles["root-container"]}>
+          <div className={styles["page-container"]}>
+            <Header />
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,37 +1,33 @@
 import styles from "./shop.module.scss";
 import { ProductItem } from "@/components/shop/product/ProductItem";
 import { shopRepository } from "@/data/repository/shopRepository";
-import { Header } from "@/components/common/Header";
 
 export default async function Shop() {
   const productList = await shopRepository.getShopProductList();
 
   return (
-    <div className={styles["page-container"]}>
-      <Header />
-      <main className={styles["main-container"]}>
-        <div className={styles["content-box"]}>
-          <p>사이토카인.</p>
-          <p>완벅한 탈모케어를 위한 선택</p>
-        </div>
-        <section className={styles["product-box"]}>
-          {productList.map((item) => {
-            return (
-              <ProductItem
-                key={item.id}
-                name={item.name}
-                description={item.description}
-                originPrice={item.originPrice}
-                price={item.price}
-                discountPercent={item.discountPercent}
-                tagInfo={item.tagInfo}
-                imageUrl={item.imageUrl}
-                productOptions={item.productOptions}
-              />
-            );
-          })}
-        </section>
-      </main>
-    </div>
+    <main className={styles["main-container"]}>
+      <div className={styles["content-box"]}>
+        <p>사이토카인.</p>
+        <p>완벅한 탈모케어를 위한 선택</p>
+      </div>
+      <section className={styles["product-box"]}>
+        {productList.map((item) => {
+          return (
+            <ProductItem
+              key={item.id}
+              name={item.name}
+              description={item.description}
+              originPrice={item.originPrice}
+              price={item.price}
+              discountPercent={item.discountPercent}
+              tagInfo={item.tagInfo}
+              imageUrl={item.imageUrl}
+              productOptions={item.productOptions}
+            />
+          );
+        })}
+      </section>
+    </main>
   );
 }
