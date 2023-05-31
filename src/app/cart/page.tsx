64d629c8a014dac8261469de09c21693792e1baa
KongWooJeong <coolsteve@naver.com>
2023-05-31 17:57:59 +0900

    feat: Add cart 페이지 기본 레이아웃 구성 및 버튼 영역 스타일 적용

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,7 +1,19 @@
+import styles from "./cart.module.scss";
+
 export default function Cart() {
   return (
-    <main>
-      <section>cart</section>
+    <main className={styles["main-container"]}>
+      <section className={styles["product-container"]}>
+        <div>cart</div>
+      </section>
+      <div className={styles["purchase-container"]}>
+        <div className={styles["button-container"]}>
+          <button className={styles.button}>
+            <span className={styles["text-medium"]}>5개 | 29,0000원 </span>
+            <span className={styles["text-bold"]}>구매하기</span>
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
