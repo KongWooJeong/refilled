5ba17518d8ec654cd11da095f5a835f45d2a7d8e
KongWooJeong <coolsteve@naver.com>
2023-06-01 13:32:14 +0900

    chore: Updagte 함수 선언 방법

--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -40,7 +40,7 @@ export function CartItem(props: CartItemProps) {
     dispatch(removeCartItem(id));
   }
 
-  const renderCartItemPriceInfo = () => {
+  function renderCartItemPriceInfo() {
     if (discountPercent === 0) {
       return (
         <div className={styles["origin-price-box"]}>
@@ -64,7 +64,7 @@ export function CartItem(props: CartItemProps) {
         </>
       );
     }
-  };
+  }
 
   return (
     <div className={styles["product-container"]}>
